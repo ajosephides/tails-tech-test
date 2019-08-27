8eca7d1b90d49ef95f12f9a014377ae27b89c0b1
Alexis Josephides <ajosephides@gmail.com>
2019-08-27 15:02:48 +0100

    appjs entry point

---
+++ app.js
@@ -0,0 +1,11 @@
+const Order = require('./src/Order')
+
+// enpoint hit and router happening in this file.
+// logic that follow on
+
+var returnPrices = function(requestData){
+  var order = new Order(requestData);
+  return order.getPrice()
+};
+
+returnPrices();

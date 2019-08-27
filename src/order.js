0d3454dba8250035e4ef388a6321438842c0a681
Alexis Josephides <ajosephides@gmail.com>
2019-08-27 16:47:45 +0100

    parse and create items

--- src/order.js
+++ src/order.js
@@ -2,6 +2,14 @@ module.exports = class Order{
 
   constructor(order){
     this.request = order;
+    this.items;
+  }
+
+  createItems(newItem = Item){
+    return this.items = this.request.order.items.map(item => {
+      var item = new newItem(item.product_id, item.quantity)
+      return item
+    })
   }
 
 }
--- tests/unit/order.test.js
+++ tests/unit/order.test.js
@@ -21,10 +21,19 @@ let mockRequest = {
   }
 }
 
+let mockItemCreater = jest.fn()
+
 describe('Order', () => {
   it('recieves an order', () => {
     var order = new Order(mockRequest)
     expect(order.request.order.id).toBe(12345)
+  })
+
+  it('creates a number of item objects', () => {
+    var order = new Order(mockRequest);
+    order.createItems(mockItemCreater);
+    expect(mockItemCreater.mock.calls.length).toBe(3);
+    expect(order.items.length).toBe(3);
   })
 
 })
